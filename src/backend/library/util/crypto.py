6269b266ddf79b8cbe15b07555abc3464462108c
thaseyor <mikeston@mail.ru>
2025-12-25 15:53:41 +0300

    feat: add v2 secret decrypt method (#4)

--- src/backend/library/util/crypto.py
+++ src/backend/library/util/crypto.py
@@ -1,3 +1,6 @@
+import base64
+import os
+
 import cryptography.hazmat.primitives.asymmetric.padding as __cryptography_padding
 import cryptography.hazmat.primitives.asymmetric.rsa as __cryptography_rsa
 import cryptography.hazmat.primitives.hashes as __cryptography_hashes
@@ -27,3 +30,31 @@ def rsa_load_pem_from_bytes(
 
 def rsa_decrypt_proto_v1(key: TRSAPrivateKey, ciphertext: bytes) -> bytes:
     return key.decrypt(ciphertext, __PROTO_V1_PADDING)
+
+
+def decrypt_api_secret_v1(private_key: TRSAPrivateKey, encrypted_secret: str) -> str:
+    if not encrypted_secret.startswith("enc_proto_v1."):
+        raise tp.WDException("Unknown encryption protocol")
+
+    _, _, key_b64 = encrypted_secret.split(".")
+    api_secret = rsa_decrypt_proto_v1(private_key, base64.b64decode(key_b64))
+
+    return bytes(api_secret).decode()
+
+
+def decrypt_api_secret_v2(encrypted_secret: str) -> str:
+    if not encrypted_secret.startswith("enc_proto_v2."):
+        raise tp.WDException("Unknown encryption protocol")
+
+    _, private_key_ref, key_b64 = encrypted_secret.split(".")
+
+    private_key_label = f"ENC_PROTO_V2_PRIVATE_KEY_B64_{private_key_ref}"
+    private_key_b64 = os.environ.get(private_key_label)
+
+    if private_key_b64 is None:
+        raise tp.WDException(f"Private key not set: {private_key_label}")
+
+    private_key = rsa_load_pem_from_bytes(base64.b64decode(private_key_b64))
+    api_secret = rsa_decrypt_proto_v1(private_key, base64.b64decode(key_b64))
+
+    return bytes(api_secret).decode()
